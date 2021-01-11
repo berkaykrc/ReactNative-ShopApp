cbfae8e9ee2a5e87701bb7c52a4338f61f069bae
berkaykrc <berkay.kirci@gmail.com>
2021-01-11 23:24:13 +0300

    homestack added

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { HomeScreen, FavoritesScreen } from "./pages";
-import { createBottomNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack'
 
-export const Tab = createBottomNavigator();
+const HomeStack = createStackNavigator();
 
+function HomeStackScreen() {
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="Home" component={HomeScreen} />
+            {/* <HomeStack.Screen name="ProductDetail" component={ProductDetailScreen} /> */}
+        </HomeStack.Navigator>
+    );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function Router() {
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name='Home' component={HomeScreen} />
+                <Tab.Screen name='Home' component={HomeStackScreen} />
                 <Tab.Screen name='Favorites' component={FavoritesScreen} />
             </Tab.Navigator>
         </NavigationContainer>
