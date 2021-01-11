b43faecb8a65523a2dc6b70d09e2937689f6c415
berkaykrc <berkay.kirci@gmail.com>
2021-01-12 01:17:49 +0300

    project structure fixed

--- src/pages/favorites/styles/favorites_styles.js
+++ src/pages/favorites/styles/favorites_styles.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native'
 
-const favorite_styles = StyleSheet.create({
-    
-})
+const favorite_styles = StyleSheet.create({})
