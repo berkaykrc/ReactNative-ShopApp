236b6e0d127664a78ab172d15cf9d053412072d1
berkaykrc <berkay.kirci@gmail.com>
2021-01-12 01:20:54 +0300

    product detail page created

--- src/Router.js
+++ src/Router.js
@@ -1,29 +1,48 @@
-import React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
-import { HomeScreen, FavoritesScreen } from "./pages";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import {
+  HomeScreen,
+  FavoritesScreen,
+  ProductDetailsScreen,
+  LogoTitle
+} from './pages'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createStackNavigator } from '@react-navigation/stack'
 
-const HomeStack = createStackNavigator();
+const HomeStack = createStackNavigator()
 
 function HomeStackScreen() {
-    return (
-        <HomeStack.Navigator>
-            <HomeStack.Screen name="Home" component={HomeScreen} />
-            {/* <HomeStack.Screen name="ProductDetail" component={ProductDetailScreen} /> */}
-        </HomeStack.Navigator>
-    );
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTitle: (props) => <LogoTitle {...props} />
+        }}
+      />
+      <HomeStack.Screen
+        name="ProductDetail"
+        component={ProductDetailsScreen}
+        options={({
+          route: {
+            params: { name }
+          }
+        }) => ({ title: `${name} Detail` })}
+      />
+    </HomeStack.Navigator>
+  )
 }
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
 export default function Router() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name='Home' component={HomeStackScreen} />
-                <Tab.Screen name='Favorites' component={FavoritesScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Favorites" component={FavoritesScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
