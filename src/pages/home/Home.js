7ed85771ef2247387d88c54683c542f445bff923
berkaykrc <berkay.kirci@gmail.com>
2021-01-12 01:19:15 +0300

    header component added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,19 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react'
+import { Button, Text, View } from 'react-native'
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
-
+      <Button
+        title="Detail"
+        onPress={() =>
+          navigation.navigate('ProductDetail', {
+            name: "Ürün adı header'a gelecek"
+          })
+        }
+      />
     </View>
-  );
+  )
 }
 
-export {HomeScreen}
+export { HomeScreen }
---
+++ src/pages/home/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, Image, View } from 'react-native'
+
+function LogoTitle() {
+  return (
+    <View>
+      <Image />
+      <Text>Test</Text>
+    </View>
+  )
+}
+
+export { LogoTitle }
