50d62dd47c38a10b08cb7c71ce8bc52a9c8fdfa8
berkaykrc <berkay.kirci@gmail.com>
2021-01-20 20:40:06 +0300

    historyscreen styled

--- src/pages/home/History.js
+++ src/pages/home/History.js
@@ -11,22 +11,33 @@ function HistoryScreen() {
     const historyList = JSON.parse(data)
     handleHistory(historyList)
   }
-
   React.useEffect(() => {
     getHistory()
-  })
+  }, [])
 
   function renderItems({ item }) {
-    console.log(item)
+    const products = item.products.map((e) => e.title)
+
     return (
-      <View style={{ flex: 1 }}>
-        <Text>{item.products.description}</Text>
+      <View style={{ flex: 1, marginVertical: 10, padding: 10 }}>
+        <Text>{products}</Text>
+        <Text style={{ alignSelf: 'flex-end' }}>
+          $ {Math.round(item.totalPrice)}
+        </Text>
       </View>
     )
   }
+  const renderHeader = () => (
+    <View style={{ borderRadius: 14, backgroundColor: '', margin: 10 }}>
+      <Text style={{ fontSize: 35, fontWeight: 'bold', margin: 5 }}>
+        Previously purchased
+      </Text>
+    </View>
+  )
   return (
     <View style={main.container}>
       <FlatList
+        ListHeaderComponent={renderHeader}
         data={history}
         keyExtractor={(_, i) => i.toString()}
         renderItem={renderItems}
